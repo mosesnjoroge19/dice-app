f06fcec1f93cac55834ff77a1993fa7700cfb200
mosesnjoroge19 <njorogemosesngure@gmail.com>
2021-03-28 02:17:28 +0300

    A flutter dice simulator

---
+++ lib/main.dart
@@ -0,0 +1,69 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          title: Text('Dicee'),
+          centerTitle: true,
+          backgroundColor: Colors.red,
+        ),
+        body: DicePage(),
+      ),
+    ),
+  );
+}
+
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  //declare variables for the image values
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 2;
+
+  @override
+  Widget build(BuildContext context) {
+    //call this function to randomise values of dice
+    void diceSetter() {
+      setState(() {
+        leftDiceNumber = Random().nextInt(6) + 1;
+        rightDiceNumber = Random().nextInt(6) + 1;
+      });
+    }
+
+    return Container(
+      child: Center(
+        child: Row(
+          children: [
+            Expanded(
+              child: TextButton(
+                onPressed: () {
+                  diceSetter();
+                },
+                child: Image(
+                  image: AssetImage('images/dice$leftDiceNumber.png'),
+                ),
+              ),
+            ),
+            Expanded(
+              child: TextButton(
+                onPressed: () {
+                  diceSetter();
+                },
+                child: Image(
+                  image: AssetImage('images/dice$rightDiceNumber.png'),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
